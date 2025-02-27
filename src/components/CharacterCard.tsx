b7331106634adae307453adbae08ca8edcbb22ab
Octadev <125304301+Octuuu@users.noreply.github.com>
2025-02-26 20:34:30 -0400

    Update CharacterCard.tsx

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -64,3 +63,5 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ id, name, image, species,
 };
 
 export default CharacterCard;
+
+
