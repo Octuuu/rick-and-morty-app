834327fe8da34f7299ec4052bb98f0bd8b0d6065
Octadev <125304301+Octuuu@users.noreply.github.com>
2025-04-14 23:19:58 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,6 @@ export const getCharacters = async () => {
     const response = await fetch('https://rickandmortyapi.com/api/character')
     const data = await response.json()
     return data
-  }
-  
+}
+
+
